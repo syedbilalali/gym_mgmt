e4e465931abd4b7cbc9e851136d8267292adc9f9
admin <admin@DESKTOP-HQ0KOBT>
2019-11-06 21:22:51 +0530

    Making the SellsOrder and SellsOrderItems

---
+++ gym_mgmt_01/Models/SellsOrder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gym_mgmt_01.Models
+{
+    public class SellsOrder
+    {
+        public int Id { get; set;  }
+        public string Invoice_number { get; set;  }
+        public string Member_Name { get; set; }
+        public decimal Subtotal { get; set; }
+        public decimal Sales_Tax { get; set;  }
+        public decimal Total_Amount { get; set;  }
+        public string Paid_Status { get; set;  }
+        public decimal Total_Pay { get; set; }
+        public decimal Discount_Price { get; set; }
+        public decimal Remain_price { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+
+    }
+}
---
+++ gym_mgmt_01/Models/SellsOrderItems.cs
@@ -0,0 +1,19 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gym_mgmt_01.Models
+{
+    public class SellsOrderItems
+    {
+        public int Id { get; set; }
+        public int Invoice_Id { get; set; }
+        public int product_Id { get; set; }
+        public decimal quantity { get; set;  }
+        public decimal unit_price { get; set;  }
+        public decimal discount_price { get; set;  }
+        public decimal total_ammount { get; set; }
+        public DateTime CreatedAt { get; set;  }
+    }
+}
